4ab733b19497932812fa983ec840520f8ad8a65c
Amiyo Chatterjee <amiyo@Amiyos-MacBook-Pro.local>
2024-09-17 09:32:53 -0400

    explicitly set cookie domain

--- app.py
+++ app.py
@@ -30,7 +30,9 @@ if app.config["ENV"] == "production":
     app.config.update(
         SESSION_COOKIE_SECURE=True,  # Only transmit cookies over HTTPS
         SESSION_COOKIE_HTTPONLY=True,  # Prevent JavaScript from accessing cookies
-        SESSION_COOKIE_SAMESITE='None'  # Lax ensures cookies are sent for the same-site requests
+        SESSION_COOKIE_SAMESITE='None',  # Lax ensures cookies are sent for the same-site requests
+        SESSION_COOKIE_DOMAIN='.onrender.com'  # Set domain explicitly
+
     )
 else:
     app.config.update(
