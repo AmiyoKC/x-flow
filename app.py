b28b41c634fb8b097b30c9656dba4dfe5c40b5da
Amiyo Chatterjee <amiyo@Amiyos-MacBook-Pro.local>
2024-09-17 09:14:21 -0400

    bug fix-  typo

--- app.py
+++ app.py
@@ -30,7 +30,7 @@ if app.config["ENV"] == "production":
     app.config.update(
         SESSION_COOKIE_SECURE=True,  # Only transmit cookies over HTTPS
         SESSION_COOKIE_HTTPONLY=True,  # Prevent JavaScript from accessing cookies
-        SESSION_COOKIE_SAMESITE='Lax'  # Lax ensures cookies are sent for the same-site requests
+        SESSION_COOKIE_SAMESITE='None'  # Lax ensures cookies are sent for the same-site requests
     )
 else:
     app.config.update(
